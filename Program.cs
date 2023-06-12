b860e427b92505e1c58df6e4020fa4f0ce705f9a
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 10:30:24 +0300

    esimerkki valmis

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+String name = "Teppo";
+Console.WriteLine("Anna nimesi");
+name=Console.ReadLine();
+Console.WriteLine("Hello, "+name+" !");
